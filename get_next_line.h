3de5c55174f7831622d99bd849803b081e4e2b8b
Kaylee Laurine <klaurine@at-p7.21-school.ru>
2019-10-21 20:37:51 +0300

    211019

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: klaurine <klaurine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/04 18:05:51 by klaurine          #+#    #+#             */
-/*   Updated: 2019/06/24 21:46:36 by klaurine         ###   ########.fr       */
+/*   Updated: 2019/10/21 20:37:26 by klaurine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 # include <fcntl.h>
 # include "libft/libft.h"
-# define FD_MAX 10240
+# define FD_MAX 4864
 # define BUFF_SIZE 9999
 
 int		get_next_line(const int fd, char **line);
